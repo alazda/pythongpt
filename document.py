140462505fc390eaf18f011c28a6c8f30db524d8
alazda <alasdairdewart@googlemail.com>
2023-05-04 01:35:48 +0100

    using document and settings object, fix new lines

--- document.py
+++ document.py
@@ -5,10 +5,6 @@ class Document :
         self.name = name
         self.text = text
         self.settings = settings
-
-    def get_all_text(self) :
-        txt = self.text.get("1.0", tk.END)
-        return txt
     
 class Settings : 
     def __init__(self, model, tokens_send, max_tokens_receive, use_max_tokens) :
@@ -16,3 +12,4 @@ class Settings :
         self.tokens_send = tokens_send
         self.max_tokens_receive = max_tokens_receive
         self.use_max_tokens = use_max_tokens
+
--- gui.py
+++ gui.py
@@ -5,28 +5,43 @@ from document import *
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
+#--settings
+model = "curie"
+use_max_tokens = False
+#--
+
+#--
 start_send = "1.0"
+#--
 
-start_doc = Document("new", "", Settings("curie", 1, 1, True))
+docs = []
 
-docs = [start_doc]
+#initialise first doc
+def initial_doc() :
+    new_doc = Document("new", "hello world!", Settings("curie", 1, 1, False))
+    docs.append(new_doc)
 
-curr_doc = 0
+initial_doc()
 
-#scripts
+current_doc = docs[0]
+#----
+
+#sending to the api
+
 def on_callback(answer):
-    text.insert(tk.END, answer)
+    c_text.insert(tk.END, answer)
     update_tokens(answer)
 
 def send_for_completion():
-    text_to_send = text.get(start_send, tk.END)
+    text_to_send = c_text.get(start_send, tk.END[:-2]).removesuffix("\n")
     print("Sending: '" + text_to_send + "'")
     answer = rq.complete(text_to_send, 'curie', tokens_retrieve.get(), on_callback)
 
-    
+#-----token calulation and such
+
 def update_tokens(event):
     #calculates the tokens
-    to_tokenize = text.get("1.0", tk.END)
+    to_tokenize = get_all_text()
 
     #print(to_tokenize)
 
@@ -41,19 +56,21 @@ def update_tokens(event):
     token_count_label.config(text = num_tokens)
 
     #the max tokens that can be used assuming a max 2048 context size
-    _max_tokens = 2048 - tokens_use.get()
+    _max_tokens = 2048 - num_tokens_use.get()
 
     #recalibrates the scalers based on the token limits and such
 
-    tokens_use.config(to=num_tokens)
+    num_tokens_use.config(to=num_tokens)
+
+    global use_max_tokens
 
     if(use_max_tokens) :
-        tokens_use.set(tokens_use.cget("to"))
+        num_tokens_use.set(num_tokens_use.cget("to"))
 
     tokens_retrieve.config(to=(_max_tokens))
 
     #total characters in the text
-    char_count = len(text.get("1.0", tk.END))
+    char_count = len(get_all_text())
 
     token_start_index = char_count
 
@@ -65,86 +82,106 @@ def update_tokens(event):
         
         token_start_index -= chars_in_token
         
-        if i <= num_tokens - tokens_use.get() :
+        if i <= num_tokens - num_tokens_use.get() :
             break
 
-    tk_pos = text.index(f'1.0 + {token_start_index} chars')
+    tk_pos = c_text.index(f'1.0 + {token_start_index} chars')
 
     #print("Pos: " + tk_pos)
 
     change_highlighted(tk_pos)
 
-    
+def remove_highlighted() :
+    c_text.tag_remove("tokens_using", start_send, tk.END)
 
 def change_highlighted(new_start_index) :
 
     global start_send
     
-    text.tag_remove("tokens_using", start_send, tk.END)
+    remove_highlighted()
     
     # Add a tag to the text to highlight tokens being used
-    text.tag_add("tokens_using", new_start_index, tk.END)
+    c_text.tag_add("tokens_using", new_start_index, tk.END)
 
     # Configure the tag to have a red foreground color
-    text.tag_config("tokens_using", foreground="lightgrey", rmargin=0)
+    c_text.tag_config("tokens_using", foreground="lightgrey", rmargin=0)
 
 
     start_send = new_start_index
 
 def change_tokens_use(event) :
-    if(tokens_use.get() >= tokens_use.cget("to") - 2):
+    update_tokens(event)
+    return
+    if(num_tokens_use.get() >= num_tokens_use.cget("to") - 2):
         use_max_tokens = False
     
     else : use_max_tokens = True
 
-    update_tokens(event)
+    
+
+#-----------------
+
+#because tkinter is a bitch
+def get_all_text() :
+    return c_text.get("1.0", tk.END[:-2])
+
+#-----saving and loading documents
 
 def save_curr_doc() : 
-    doc_text = text.get("1.0", tk.END)
-    docs_content[curr_doc] = doc_text
+    global model, use_max_tokens
+    current_doc.text = get_all_text()
+    current_doc.settings = Settings(model, num_tokens_use.get(), tokens_retrieve.get(), use_max_tokens)
 
 def change_document(event) :
     save_curr_doc()
-
-    selected_name = get_selected_doc_name()
-
-    text.delete("1.0", tk.END)
-
-    text.insert("1.0", docs_content[selected_name])
-
-    global curr_doc
-
-    curr_doc = selected_name
+    index = document_selector.curselection()[0]
+    print("doc index: " + str(index))
+    load_doc(index)
     return
-
-def get_selected_doc_name() :
-    sname = document_selector.get(document_selector.curselection())
-    print(" doc name: " + sname)
-    return sname
 
 def new_document() :
     save_curr_doc()
 
-    new_doc_name = "new(1)"
+    new_doc = Document("new2", "hello world2!", Settings("curie", 1, 1, False))
 
-    for i in range(1, 100):
-        key = "new(" + str(i) + ")"
-        if key in docs_content :
-            continue
-        else :
-            new_doc_name = key
-            break
+    docs.append(new_doc)
 
-    docs_content[new_doc_name] = "well, it worked, you fool!"
-    litems = list(doc_list.get())
-    litems.append(new_doc_name)
-    doc_list.set(litems)
-    global curr_doc 
-    curr_doc = new_doc_name
+    document_selector.insert(tk.END, new_doc.name)
+
+    load_doc(len(docs) - 1)
     return
 
-def load_doc() :
-    text.insert("1.0", docs_content[curr_doc])
+def load_doc(index) :
+    global current_doc, settings
+
+    current_doc = docs[index]
+    settings = current_doc.settings
+
+    load_settings()
+
+    remove_highlighted()
+
+    start_send = "1.0"
+
+    c_text.delete("1.0", tk.END)
+    c_text.insert("1.0", current_doc.text)
+    c_text.delete(tk.END + "-1c")
+
+    update_tokens(None)
+
+def load_settings() :
+    global settings, model, use_max_tokens
+
+    model = settings.model
+    use_max_tokens = settings.use_max_tokens
+
+    num_tokens_use.set(settings.tokens_send)
+    tokens_retrieve.set(settings.max_tokens_receive)
+
+    
+
+
+#-----------------
 
 #-------------building the gui
 
@@ -158,25 +195,26 @@ left_frame.pack(side = tk.LEFT)
 
 token_count_label = tk.Label()
 
-text = tk.Text(
+c_text = tk.Text(
     wrap=tk.WORD,
-    foreground='white',
-    background='black',
+    foreground='black',
+    background='white',
     width=100,
     height=30
 )
 
 #making the doc tabs
 
-dlistnames = ["new", "new(1)"]
+document_selector = tk.Listbox(left_frame, height = 10)
 
-doc_list = tk.Variable(value=dlistnames)
+document_selector.bind("<<ListboxSelect>>", change_document)
 
-document_selector = tk.Listbox(left_frame, height = 5, listvariable=doc_list)
+for _doc in docs :
+    document_selector.insert(tk.END, _doc.name)
 
 #---
 
-text.tag_add("tokens_using", start_send, tk.END)
+c_text.tag_add("tokens_using", start_send, tk.END)
 
 
 newbutton = tk.Button(left_frame,
@@ -198,8 +236,8 @@ button = tk.Button(bottom_frame,
 #token scales
 
 #this is the slider to indicate how many of the tokens in the document should be sent off
-tokens_use = tk.Scale(bottom_frame, from_=1, to=1, orient=tk.HORIZONTAL)
-tokens_use.pack(side = tk.RIGHT)
+num_tokens_use = tk.Scale(bottom_frame, from_=1, to=1, orient=tk.HORIZONTAL)
+num_tokens_use.pack(side = tk.RIGHT)
 
 
 #this is the slider of how many tokens the user wants to get back
@@ -207,9 +245,9 @@ tokens_retrieve = tk.Scale(bottom_frame, from_=1, to=2048, orient=tk.HORIZONTAL)
 tokens_retrieve.pack(side = tk.RIGHT)
 
 #event handlers
-tokens_use.bind("<B1-Motion>", change_tokens_use)
-text.bind("<KeyRelease>", update_tokens)
-document_selector.bind("<<ListboxSelect>>", change_document)
+num_tokens_use.bind("<B1-Motion>", change_tokens_use)
+c_text.bind("<KeyRelease>", update_tokens)
+
 
 #packing
 
@@ -217,11 +255,11 @@ newbutton.pack()
 document_selector.pack()
 
 token_count_label.pack()
-text.pack()
-tokens_use.pack()
+c_text.pack()
+num_tokens_use.pack()
 
 button.pack()
 
-load_doc()
+load_doc(0)
 
 root.mainloop()
